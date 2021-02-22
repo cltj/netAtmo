dddfb3117446aa54c6c0a8a8f10a42257a32f39d
TJ <kinstad85@gmail.com>
2021-02-22 10:31:04 +0100

    added rough plan

--- src/netatmo/main.py
+++ src/netatmo/main.py
@@ -2,6 +2,10 @@
 
 import netatmo
 import fs
+import requests
+import json
+import time
+
 
 # fetch data using ~/.netatmorc credentials
 netatmo.fetch()
@@ -16,8 +20,21 @@ ws = netatmo.WeatherStation( {
 ws.get_data()
 print(ws.devices)
 
+# Open file 
 
-    
+# Get list
+
+# Format list values
+
+# Append values to JSON file
+
+# close file
+
+# Check if temp is within bounds
+
+# If not send POST req to sysman
+
+
 #import datetime
 #readable = datetime.datetime.fromtimestamp(1613792951).isoformat()
 #print(readable)
